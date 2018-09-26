2d2e14453a73d9a5b39a2a25c7f1f6643874a2ad
Tobias Stenzel <t.stenzel@alphanodes.com>
2018-09-26 07:44:36 +0000

    replace 'all words' label with a better description

---
+++ app/overrides/search/index.rb
@@ -0,0 +1,5 @@
+Deface::Override.new virtual_path:  'search/index',
+                     name:          'search-index-replace-all-words-label',
+                     replace:       'erb[loud]:contains("l(:label_all_words)")',
+                     original:      '6c8eac970598de7d3f558fd0f77c772962b8fd76',
+                     partial:       'search/postgresql_search_index'
