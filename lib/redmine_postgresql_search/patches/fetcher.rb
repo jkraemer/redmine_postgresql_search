cd276b1c16cd253ee115a3b929e719ad68e95e28
Tobias Stenzel <t.stenzel@alphanodes.com>
2018-10-02 05:12:42 +0000

    fix error when only searchables without Postgres support are searched

--- lib/redmine_postgresql_search/patches/fetcher.rb
+++ lib/redmine_postgresql_search/patches/fetcher.rb
@@ -30,11 +30,16 @@ module RedminePostgresqlSearch
           scope_options[scope][:last_modification_field] = klass.last_modification_field
         end
 
-        sql = @query_builder.search_sql(queries_with_scope, scope_options)
-        result = ActiveRecord::Base.connection.execute(sql)
-        # with Redmine 3, id is returned as a string
-        # with Redmine 4, an int is returned
-        pg_matches = result.each_row.map { |scope, id| [scope, id.to_i] }
+        pg_matches =
+          if queries_with_scope.present?
+            sql = @query_builder.search_sql(queries_with_scope, scope_options)
+            result = ActiveRecord::Base.connection.execute(sql)
+            # with Redmine 3, id is returned as a string
+            # with Redmine 4, an int is returned
+            result.each_row.map { |scope, id| [scope, id.to_i] }
+          else
+            []
+        end
         # Use Redmine's default search as fallback if a type has no config for Postgres
         # and append them to the PostgreSQL fts search results.
         # This means that other results will be ranked lower than all fts results.
